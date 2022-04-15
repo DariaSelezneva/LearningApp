6ff37b34af1fa32d330a57ea4a2acfde5f41869e
DariaSe <daria.samosledova@icloud.com>
2022-04-15 10:55:13 +0300

    Added missed files

---
+++ LearningApp/Extensions/e_UIColor.swift
@@ -0,0 +1,21 @@
+//
+//  e_UIColor.swift
+//  LearningApp
+//
+//  Created by Дарья Селезнёва on 15.04.2022.
+//
+
+import UIKit
+
+extension UIColor {
+    static let appDarkGray = UIColor(named: "darkGray")
+    static let appGray = UIColor(named: "gray")
+    static let appLightGray = UIColor(named: "lightGray")
+    static let appRed = UIColor(named: "red")
+    static let appYellow = UIColor(named: "yellow")
+    static let appBlue = UIColor(named: "blue")
+    static let appGreen = UIColor(named: "green")
+    static let appMint = UIColor(named: "Mint")
+    static let appPurple = UIColor(named: "purple")
+}
+
---
+++ LearningApp/ViewModel/LessonViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  LessonViewModel.swift
+//  LearningApp
+//
+//  Created by Дарья Селезнёва on 15.04.2022.
+//
+
+import Foundation
+
+struct LessonViewModel {
+    
+    var title: String
+    var duration: String
+    
+    static let sample = [LessonViewModel(title: "Brief history of TARDIS: from Gallifrey war to today", duration: "17:10m"), LessonViewModel(title: "Quantum physics basics", duration: "34:17m")]
+}
